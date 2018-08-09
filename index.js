cd2101d0ff5e4b6504c9108bc83f4be5fbb837ca
stadibo <jesperleonard@gmail.com>
2018-08-09 22:15:10 +0300

    Initiate app to heroku.

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.put('/api/persons/:id', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
