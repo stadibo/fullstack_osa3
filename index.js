ad5a80048d065bd2a03abbf0c2b1408e09450421
stadibo <jesperleonard@gmail.com>
2018-08-09 16:53:29 +0300

    3.1-6 done

--- index.js
+++ index.js
@@ -26,3 +26,70 @@ let persons = [
         "id": 4
     }
 ]
+
+app.get('/api', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+            <div>
+                <p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+                <p>${new Date()}</p>
+            </div>`
+    )
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000000)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    const nameTaken = persons.find(p => p.name === body.name)
+
+    if ( !body.name ) {
+        return res.status(400).json({ error: 'name missing' })
+    } else if ( !body.number ) {
+        return res.status(400).json({ error: 'number missing' })
+    } else if ( nameTaken ) {
+        return res.status(400).json({ error: 'name must be unique' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
